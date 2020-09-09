7f4c3b9b8da70374ec01a30600fcf95e91d56c31
roninby <roninby@yeah.net>
2020-09-09 12:26:02 +0800

    fluent-bit changed

--- flux/resources/metallb-system/generate-secret.sh
+++ flux/resources/metallb-system/generate-secret.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)" -o yaml --dry-run=client > metallb-secret.yaml
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)" -o yaml --dry-run > metallb-secret.yaml
